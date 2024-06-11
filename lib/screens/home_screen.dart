429b41f50d178bea854fa8d4a2b224075f8562db
Murat Alperen Ulutaş <153949398+muratalperenulutas@users.noreply.github.com>
2024-06-11 18:38:35 +0300

    added scrollable page feature

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,7 +10,8 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage>  with TickerProviderStateMixin {
+  late final TabController _tabController;
   int _selectedIndex = 0;
   void _onItemTapped(int index) {
     setState(() {
@@ -18,12 +19,25 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+    @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 4, vsync: this);
+  }
+
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
+
+    List<Widget> widgetOptions = <Widget>[
+      const Center(child: Text("Home Page")),
+      const Center(child: Text("Chats Page")),
+      const Center(child: Text("Contacts Page")),
+      const Center(child: Text("Me Page"))
+    ];
     return Scaffold(
-      body: buildChatsBody(screenHeight),
+      body: TabBarView(controller: _tabController, children: widgetOptions),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         child: Icon(Icons.message),
