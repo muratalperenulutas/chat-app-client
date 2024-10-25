b15d234d6d972a201c4917b4556d5adeee7a899c
Murat Alperen Ulutaş <153949398+muratalperenulutas@users.noreply.github.com>
2024-10-25 18:56:12 +0300

    added url config file

---
+++ lib/config/url.dart
@@ -0,0 +1,15 @@
+
+String address1 = "https://";
+String address2 = "http://localhost:8090";
+String address3 = "http://10.0.2.2:8090";
+
+String serverAddress = address3;
+
+class Url {
+  static final auth = Auth();
+}
+
+class Auth{
+  String Login = '$serverAddress/login';
+  String Register = '$serverAddress/register';
+}
