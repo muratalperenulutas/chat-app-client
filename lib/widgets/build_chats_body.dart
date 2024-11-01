26ffe568bb0d41020939b8094274b082359c8a11
Murat Alperen Ulutaş <153949398+muratalperenulutas@users.noreply.github.com>
2024-11-01 13:24:00 +0300

    fix

--- lib/widgets/build_chats_body.dart
+++ lib/widgets/build_chats_body.dart
@@ -2,7 +2,8 @@ import 'package:chat_app/models/group.dart';
 import 'package:chat_app/widgets/my_conversation_button.dart';
 import 'package:flutter/material.dart';
 
-Widget buildChatsBody(double screenHeight,Future<List<GroupModel>> groupsFuture) {
+Widget buildChatsBody(
+    double screenHeight, Future<List<GroupModel>> groupsFuture) {
   return FutureBuilder<List<GroupModel>>(
     future: groupsFuture,
     builder: (context, snapshot) {
@@ -16,12 +17,16 @@ Widget buildChatsBody(double screenHeight,Future<List<GroupModel>> groupsFuture)
         final groups = snapshot.data!;
         return SingleChildScrollView(
           child: Column(
-            children:[ListView.builder(
-          itemCount: groups.length,
-          itemBuilder: (context, index) {
-            return myConversationButton(screenHeight,groups[index]);
-          },
-        )],
+            children: [
+              ListView.builder(
+                shrinkWrap: true,
+                primary: false,
+                itemCount: groups.length,
+                itemBuilder: (context, index) {
+                  return myConversationButton(screenHeight, groups[index]);
+                },
+              )
+            ],
           ),
         );
       }
